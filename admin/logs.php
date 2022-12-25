bf8b726b45a8fa1cef607ef71989be35cdec837d
jbalansa143 <jbalansa143@gmail.com>
2022-12-25 09:24:33 +0800

    add echo in logs line: 9

--- admin/logs.php
+++ admin/logs.php
@@ -6,6 +6,7 @@ if (isset($_SESSION["id"])) :
     $user_id = $_SESSION["id"];
     include("../connections.php");
     include("fetch.php");
+    echo $user_id;
 ?>
     <!DOCTYPE html>
     <html lang="en">
